5802f6d256956d0e5f7af11b48f69bbbe5784c38
Key <86489161+Kyluesky@users.noreply.github.com>
2021-10-22 08:52:01 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,47 +1,11 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const PhoneNumber = require('awesome-phonenumber')
-async function handler(m) {
-  let name = 'Key Baka~'
-  number = owner[0].replace(/[^0-9]/g, '')
-  let njid = number + '@s.whatsapp.net'
-  let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
-
-  let name2 = 'ElainaChan'
-  number2 = owner[1].replace(/[^0-9]/g, '')
-  let njid2 = number2 + '@s.whatsapp.net'
-  let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-
-  this.sendMessage(m.chat, {
-    contacts: [{
-      displayname: name, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name.replace(/\n/g, '\\n')};;;
-FN:${name.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}${onW.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid].vname || this.getName(njid)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name2, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name2.replace(/\n/g, '\\n')};;;
-FN:${name2.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number2}:${PhoneNumber('+' + number2).getNumber('international')}${onW2.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid2].vname || this.getName(njid2)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid2)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }]
-  }, MessageType.contactsArray, { quoted: m })
+function handler(m) {
+  // Ini cuma contoh, jgn di uncomment -_-
+  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
+  this.sendContact(m.chat, '6285808936396', 'Key Baka~', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
-handler.command = /^(nowner)$/i
+handler.command = /^(owner|creator)$/i
 
 module.exports = handler
